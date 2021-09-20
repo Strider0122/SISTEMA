6a7d1184b430165d3eb59c25b22d0b263453f44f
Strider0122 <76915351+Strider0122@users.noreply.github.com>
2021-09-20 16:22:10 -0500

    Update settings.py

--- PRACTICAS_TESIS/settings.py
+++ PRACTICAS_TESIS/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_base_db',
         'USER': 'djangouser',
-        'PASSWORD': 'password,
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
     }
